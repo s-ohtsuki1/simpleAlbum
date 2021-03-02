753b5c3a5ba0c9116368fc3b3d216e4beb85a189
s-ohtsuki1 <secom.24h@gmail.com>
2021-03-02 16:12:25 +0900

    設定を変更

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:favorite/presentaition/book_list/main/main.dart';
+import 'package:favorite/presentaition/main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
