641bcffacfe26dd840c7830987e0a1f997859725
s-ohtsuki1 <secom.24h@gmail.com>
2021-03-09 12:00:16 +0900

    会員登録画面コンポーネント追加

---
+++ lib/components/no_account_text.dart
@@ -0,0 +1,32 @@
+import 'package:favorite/constants.dart';
+import 'package:favorite/screen/forgot_password/forgot_password_screen.dart';
+import 'package:favorite/screen/sign_up/sign_up_screen.dart';
+import 'package:favorite/size_config.dart';
+import 'package:flutter/material.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("アカウントをお持ちではないですか？",
+            style: TextStyle(fontSize: getProportionateScreenWidth(16))),
+        GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SignUpScreen()),
+          ),
+          child: Text("会員登録",
+              style: TextStyle(
+                  fontSize: getProportionateScreenWidth(16),
+                  color: kPrimaryColor)),
+        ),
+      ],
+    );
+  }
+}
