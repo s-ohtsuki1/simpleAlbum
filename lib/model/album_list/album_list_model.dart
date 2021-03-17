8b86404ec36716b8e6b6dacd1adc658e66d06d25
s-ohtsuki1 <secom.24h@gmail.com>
2021-03-17 23:34:14 +0900

    entity名変更

--- lib/model/album_list/album_list_model.dart
+++ lib/model/album_list/album_list_model.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:favorite/entity/album.dart';
 import 'package:flutter/material.dart';
 
-import '../book.dart';
-
 class AlbumListModel extends ChangeNotifier {
-  List<Book> books = [];
+  List<Album> books = [];
   bool isLoading = false;
 
   startLoading() {
@@ -20,13 +19,13 @@ class AlbumListModel extends ChangeNotifier {
   // 本一覧取得
   Future fetchBooks() async {
     final docs = await Firestore.instance.collection('books').getDocuments();
-    final books = docs.documents.map((doc) => Book(doc)).toList();
+    final books = docs.documents.map((doc) => Album(doc)).toList();
     this.books = books;
     notifyListeners();
   }
 
   // 本の削除
-  Future deleteBook(Book book) async {
+  Future deleteBook(Album book) async {
     await Firestore.instance
         .collection('books')
         .document(book.documentId)
