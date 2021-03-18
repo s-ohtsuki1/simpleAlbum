457066547e55c9e1d74f6237a19c9f430e8944ee
s-ohtsuki1 <secom.24h@gmail.com>
2021-03-18 23:36:15 +0900

    写真お気に入りメソッド作成

--- lib/model/album_detail/album_detail_model.dart
+++ lib/model/album_detail/album_detail_model.dart
@@ -1,10 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:favorite/entity/favorite.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AlbumDetailModel extends ChangeNotifier {
-  int userId = 0;
-  String picDocumentId = '';
-  bool isFavorite = false;
+  List<Favorite> favorit = [];
+  String favoriteId;
+  FirebaseUser user;
+  // String picDocumentId = '';
+  bool isFavorite;
 
   changeFavorite() {
     if (isFavorite)
@@ -15,15 +19,45 @@ class AlbumDetailModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  // お気に入り情報取得
+  Future fetchFavorite(String picDocumentId) async {
+    this.favorit = [];
+    user = await FirebaseAuth.instance.currentUser();
+    final docs = await Firestore.instance
+        .collection('favorites')
+        .where("userId", isEqualTo: user.uid)
+        .where("picDocumentId", isEqualTo: picDocumentId)
+        .limit(1)
+        .getDocuments();
+    this.favorit = docs.documents.map((doc) => Favorite(doc)).toList();
+    if (favorit.length == 1) {
+      isFavorite = true;
+      favoriteId = this.favorit.first.documentId;
+    } else {
+      isFavorite = false;
+    }
+    notifyListeners();
+  }
+
   // お気に入り追加
-  Future saveFavorite() async {
-    if (isFavorite)
+  Future saveFavorite(String picDocumentId, bool preIsFavorite) async {
+    if (!preIsFavorite && isFavorite) {
+      user = await FirebaseAuth.instance.currentUser();
+
       await Firestore.instance.collection('favorites').add(
         {
-          'userId': userId,
+          'userId': user.uid,
           'picDocumentId': picDocumentId,
           'createdAt': Timestamp.now(),
         },
       );
+    }
+    if (preIsFavorite && !isFavorite) {
+      await Firestore.instance
+          .collection('favorites')
+          .document(favoriteId)
+          .delete();
+    }
+    notifyListeners();
   }
 }
