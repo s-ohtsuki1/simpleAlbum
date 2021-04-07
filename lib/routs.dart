c61f809f126d69bca0db2845d0cb678b939cbb84
s-ohtsuki1 <secom.24h@gmail.com>
2021-04-07 22:11:44 +0900

    ルートファイルを削除

--- lib/routs.dart
+++
@@ -1,9 +0,0 @@
-import 'package:favorite/screen/forgot_password/forgot_password_screen.dart';
-import 'package:favorite/screen/sign_in/sign_in_screen.dart';
-import 'package:flutter/widgets.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  // SplashScreen.routeName: (context) => SplashScreen();
-  SignInScreen.routeName: (context) => SignInScreen(),
-  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
-};
